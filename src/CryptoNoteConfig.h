63a4cbb3c8cc3957c0674bd2094f9d553a42cbfe
pridilla <ptrridilla9@gmail.com>
2018-07-04 06:55:38 +0000

    Update CryptoNoteConfig.h
    Pridaný genesis blok.

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,8 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "roxorcoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char GENESIS_COINBASE_TX_HEX[] = "013c01ff0001c08f8af8ae5f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121011e616eb6ac7c10f5d53844da3197fa4bfcb0b9c1c6c378549c800785699704f6";
+
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
